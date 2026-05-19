e3104409603b18159afe96f8f635e6974913676d
Adam Jattiem <adamjattiem12@gmail.com>
2026-05-19 11:19:07 +0200

    Add files via upload

---
+++ Intro to JavaScript Exercise 2/Exercise 2 operations-types.js
@@ -0,0 +1,8 @@
+let number1 = 10
+let number2 = 15
+let number1String = String(number1);
+let sum = number1 + number2;
+let product = number1 * number2;
+console.log(`number1 = ${number1}, number2 = ${number2}, number1String = ${number1String}, sum = ${sum}, product = ${product}`);
+
+
